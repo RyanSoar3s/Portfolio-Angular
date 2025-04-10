1fdbd147e76a88357e6202a33d7580a525b9e0e7
RyanSoar3s <ryansoares.dev@gmail.com>
2025-04-10 14:30:48 -0300

    test: add case tests

--- meu-portfolio/src/app/components/navigation/navigation.component.spec.ts
+++ meu-portfolio/src/app/components/navigation/navigation.component.spec.ts
@@ -9,6 +9,7 @@ describe('NavigationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavigationComponent]
+
     })
     .compileComponents();
 
@@ -17,7 +18,49 @@ describe('NavigationComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 1200
+
+    });
+    fixture.detectChanges();
+
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+
   });
+
+  it("should click on the button when the screen is small", () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 650
+    });
+
+    window.dispatchEvent(new Event('resize'));
+    component.isSmallScreen = true;
+    fixture.detectChanges();
+
+    const menuButton = fixture.nativeElement.querySelector(".container-menu-icon");
+    menuButton.click();
+    fixture.detectChanges();
+
+    expect(component.hidden_menu).toBeTruthy();
+
+  });
+
+
+  it("should click on the image", () => {
+    const image = fixture.nativeElement.querySelector(".container-img");
+    console.log(image);
+    image.click();
+    fixture.detectChanges();
+    expect(component.isZoomImage).toBeTruthy();
+
+  });
+
 });
