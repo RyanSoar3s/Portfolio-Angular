6d6a3d74ea361d9e733c85e9bb2499fb042acb54
RyanSoar3s <ryansoares.dev@gmail.com>
2025-04-28 19:30:13 -0300

    chore: remove Animations Module

--- meu-portfolio/src/app/app.config.ts
+++ meu-portfolio/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  BrowserAnimationsModule,
+  provideAnimations
 
-import { routes } from './app.routes';
+} from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes'
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom([ BrowserAnimationsModule, BrowserModule  ])
+
+  ]
 };
