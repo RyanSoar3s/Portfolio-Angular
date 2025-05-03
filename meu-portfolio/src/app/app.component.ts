f673254cc5215440b705c630a4cc19ab1ccaa429
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-03 13:00:36 -0300

    chore: add projects component and add custom component paths

--- meu-portfolio/src/app/app.component.ts
+++ meu-portfolio/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { HomeComponent } from '@components/home/home.component';
+import { AboutMeComponent } from '@components/about-me/about-me.component';
+import { ProjectsComponent } from '@components/projects/projects.component';
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     HomeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ProjectsComponent
 
   ],
   templateUrl: './app.component.html',
