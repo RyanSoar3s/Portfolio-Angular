6742b697a7db7fcd221c44cc60a88e416e275e59
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-03 12:54:34 -0300

    feat: add project-info type for use in project information made

---
+++ meu-portfolio/src/app/types/project-info.d.ts
@@ -0,0 +1,11 @@
+export type ProjectInfo = {
+  id: number;
+  path: string;
+  altText: string;
+  title: string;
+  isDisabledButtonProject: boolean;
+  isDisabledButtonRepo: boolean;
+  linkProject: string;
+  linkRepo: string;
+
+}
