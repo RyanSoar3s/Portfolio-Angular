e436c43ec48548d5a9b5aa96c47094283cc0cb14
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-06 14:50:53 -0300

    feat: add app-contacts

--- meu-portfolio/src/app/app.component.ts
+++ meu-portfolio/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { HeaderComponent } from '@components/header/header.component';
 import { HomeComponent } from '@components/home/home.component';
 import { AboutMeComponent } from '@components/about-me/about-me.component';
 import { ProjectsComponent } from '@components/projects/projects.component';
+import { ContactsComponent } from '@components/contacts/contacts.component';
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     HomeComponent,
     AboutMeComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactsComponent
 
   ],
   templateUrl: './app.component.html',
