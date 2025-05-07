9a553b16571013289637959868ca09f158a08302
RyanSoar3s <ryansoares.dev@gmail.com>
2025-05-07 20:40:01 -0300

    feat: create footer component

---
+++ meu-portfolio/src/app/components/footer/footer.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faGithub,
+  faLinkedin,
+  faWhatsapp
+
+} from '@fortawesome/free-brands-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+
+],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  protected readonly faGithub = faGithub;
+  protected readonly faLinkedin = faLinkedin;
+  protected readonly faWhatsapp = faWhatsapp;
+  protected readonly faAt = faAt;
+
+}
